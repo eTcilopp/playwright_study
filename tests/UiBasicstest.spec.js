a7ef071fedaf67f77afc75742942dd210376f22c
etcilopp <kirikeza@gmail.com>
2023-11-06 19:36:27 -0700

    lesson 10 done

--- tests/UiBasicstest.spec.js
+++ tests/UiBasicstest.spec.js
@@ -1,13 +1,18 @@
-const {test} = require("@playwright/test");
+const {test, expect} = require("@playwright/test");
 
 test('Browser Context Declaration Playwright test', async ({browser})=> 
 {
     const context = await browser.newContext();
     const page = await context.newPage();
     await page.goto("https://ibm.com/");
+    console.log(await page.title());
 });
 
-test('PagePlaywright test', async ({page})=> 
+test.only('PagePlaywright test', async ({page})=> 
 {
     await page.goto("https://ibm.com/");
+    // get title + assert
+    await page.title();
+    await expect(page).toHaveTitle("IBM - Canada");
+    
 });
