66ab362d7773b0bd00ea833b0de190e7699bbb59
eTcilopp <kirikeza@gmail.com>
2023-11-07 20:01:07 -0700

    completed lesson 13

--- tests/UiBasicstest.spec.js
+++ tests/UiBasicstest.spec.js
@@ -8,16 +8,35 @@ test('Browser Context Declaration Playwright test', async ({browser})=>
     console.log(await page.title());
 });
 
-test.only('PagePlaywright test', async ({page})=> 
+test('PagePlaywright test', async ({page})=> 
 {
-    test.setTimeout(60000); 
+    test.setTimeout(60000);
+    // const context = await browser.newContext();
+    // const page = await context.newPage();
+
+    const userNameInput =  page.locator('input[name="username"]');
+    const userPasswordInput =  page.locator('input[name="password"]');
+    const submitButton = page.locator('input[type="submit"]');
+
     await page.goto("https://cfo-uat.api.vitruvi.cc/admin/login/?next=/admin/");
-    // get title + assert
+
     console.log(await page.title());
     await expect(page).toHaveTitle("Log in | Vitruvi Admin");
-    await page.locator('input[name="username"]').fill("Alexamder.Kirikeza@vitruvisoftware.com");
-    await page.locator('input[name="password"]').fill("P@ssw0rd!");
-    await page.locator('input[type="submit"]').click();
+
+    await userPasswordInput.fill("P@ssw0rd!");;
+    await submitButton.click();
+
+    await expect(userNameInput).toHaveAttribute('required', '');
+    const message = await page.evaluate(el => el.validationMessage, await userNameInput.elementHandle());
+    expect(message).toContain('Please fill out this field');
+
+    await userNameInput.fill("Alexander.Kirikeza@vitruvisoftware.com");
+    await userPasswordInput.fill("P@ssw0rd!");
+    await submitButton.click();
+    
+    const section = page.locator(".section")
+    console.log(await section.first().textContent());
+    console.log(await section.nth(2).textContent());
     console.log(await page.title());
 
 });
