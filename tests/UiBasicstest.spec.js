d3f5a5c24cac2b7f89debae9bddb13aa3367f86a
eTcilopp <kirikeza@gmail.com>
2023-11-09 19:37:45 -0700

    lesson 20 done

--- tests/UiBasicstest.spec.js
+++ tests/UiBasicstest.spec.js
@@ -54,7 +54,7 @@ test('PagePlaywright test', async ({page})=>
 });
 
 
-test.only('UI Control test', async ({page})=> 
+test('UI Control test', async ({page})=> 
 {
 
     // const context = await browser.newContext();
@@ -64,6 +64,7 @@ test.only('UI Control test', async ({page})=>
     const userPasswordInput =  page.locator('input[name="password"]');
     const submitButton = page.locator('input[type="submit"]');
     const section = page.locator(".section")
+    const firstCheckbox = page.locator(".action-select").first()
 
     // await page.goto("https://cfo-uat.api.vitruvi.cc/admin/login/?next=/admin/");
     await page.goto("http://10.0.0.16:8000/admin/login/?next=/admin/");
@@ -76,9 +77,17 @@ test.only('UI Control test', async ({page})=>
     const dropdown =   page.locator("[name='action']");
     await dropdown.selectOption("Delete selected custom actions");
 
-    await page.locator(".action-select").first().check();
+    await firstCheckbox.check();
+    await expect(firstCheckbox).toBeChecked();
     await page.locator('.button').click();
     await page.locator("[class='button cancel-link']").click();
+
+    await firstCheckbox.uncheck();
+    // no assertion for uncheck() BUT
+    expect(await firstCheckbox.isChecked()).toBeFalsy();
+
+    const testLink = page.locator("[title*='Content Types']");
+    await expect(testLink).toHaveAttribute("class", "section");
     await page.pause();
 
     
@@ -87,9 +96,35 @@ test.only('UI Control test', async ({page})=>
     await page.locator("#content").waitFor()
     
     
-
-    // Getting an array
-    // const allSections = await section.allTextContents();
-    // console.log(allSections);
-
 });
+
+test.only('Lesson 20 test Child window', async ({browser})=> 
+{
+    // проблема состоит в том, что иногда при клике на ссылку открывается новое окно (новая вкладка)
+    // чтобы работать с несколькими вкладками, нужно
+    // передать в тест не страницу, а браузер (см. выше ---> test Child window', async ({browser})=>)
+    // уже в браузере создать контекст
+    const context = await browser.newContext();
+    // в этом контексте создать новую страницу
+    const page = await context.newPage();
+    // найти ссылку как обычно
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+    const documentLink = page.locator("[href*='documents-request']");
+    
+    // далее нужно поймать новую страницу, соединив кликание на по ссылке и открытие страницы
+    const [newPage] = await Promise.all([
+        context.waitForEvent('page'),
+        documentLink.click(),
+    ]);
+    // дальше обрабатывать как обычно.
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@");
+    const retrievedUsername = text.split("@")[1].split(" ")[0].split(".")[0];
+    console.log(retrievedUsername);
+
+    // тут важно: newPage - это новая открывшаяся страница, 
+    // page - старая родительская.
+    await page.locator('input[name="username"]').fill(retrievedUsername);
+    await page.pause();
+
+})
