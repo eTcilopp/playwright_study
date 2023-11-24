b954cb639ab464749cc62800c20d6544a2261065
eTcilopp <kirikeza@gmail.com>
2023-11-23 18:34:53 -0700

    added APiUtils fie

--- tests/WebAPIPart1.spec.js
+++ tests/WebAPIPart1.spec.js
@@ -1,29 +1,30 @@
 const {test, expect, request} = require("@playwright/test");
+
 const loginPayload = {"userEmail":"tcilop-p@yandex.ru","userPassword":"Iamking@123"}
+let token;
 
 test.beforeAll(async ()=> 
 {
     const apiContext = await request.newContext();
     const loginResponse = await apiContext.post("https://rahulshettyacademy.com/api/ecom/auth/login", {data:loginPayload});
     expect(loginResponse.ok()).toBeTruthy();
-    const loginResponseJson = loginResponse.json();
-    const token = loginResponseJson.token;
-    console.log(loginResponseJson);
+    const loginResponseJson = await loginResponse.json();
+    token = loginResponseJson.token;
 
 });
 
-// test.beforeEach(()=>
-// {
 
-// });
-
-test('PagePlaywright test', async ({page})=> 
+test('Client App login', async ({page})=> 
 {
-
+    page.addInitScript(value => {
+        window.localStorage.setItem('token', value);
+    }, token);
     await page.goto("https://rahulshettyacademy.com/client/");
     console.log(await page.title());
-    await page.locator("#userEmail").fill("tcilop-p@yandex.ru");
-    await page.locator("#userPassword").fill("Iamking@123");
-    await page.locator("[value='Login']").click();
 
 });
+
+test('Place the order', async ({page})=>{
+    const APIUtils = new APIUtils(apiContext, loginPayLoad);
+    const orderId = createOrder(orderPayload);
+})
